81f0bae501288ff75e819ecc5f7bd76b4c310052
NienkedeGroot <61182256+NienkedeGroot@users.noreply.github.com>
2020-05-15 11:31:26 +0200

    Poging honderd

---
+++ 04Animation/scripts/main.js
@@ -0,0 +1,14 @@
+
+
+// // changing individual properties with code and using setInterval
+var rotationSpeed = 0.01;
+var myOtherBox = document.getElementById('myOtherBox');
+
+function spin(){
+	
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
+
+myOtherBox.object3D.rotation.x += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
