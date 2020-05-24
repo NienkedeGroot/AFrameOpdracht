468ec1199af15ba7426d405ebea2c423a53bfa60
NienkedeGroot <61182256+NienkedeGroot@users.noreply.github.com>
2020-05-24 20:05:57 +0200

    j

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -34,12 +34,12 @@ function grow(){
 myOtherBox.object3D.scale.x *= growspeed;
 myOtherBox.object3D.scale.y *= growspeed;
 myOtherBox.object3D.scale.z *= growspeed;
-console.log(myOtherBox.object3D.scale);
+//console.log(myOtherBox.object3D.scale);
 }
 
 myOtherBox.addEventListener('click', function(){ // uses a fuse
 grow();
-console.log('grew');
+//console.log('grew');
 });
 
 // var yas = 0.02;
